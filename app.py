3d671954b3b5466946c0312b2c14f953bf20ca17
Grzegorz Jarosiński <129977521+grzegjar@users.noreply.github.com>
2025-11-22 20:13:41 +0100

    Poprawka

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ import streamlit as st
 from datetime import date
 from config import MIEJSCOWOSC, LAT, LON
 
-st.set_page_config(page_title="Biometria – Formularz", layout="wide")
+st.set_page_config(page_title="Przewidywanie bólu", layout="wide")
 
-st.title("Formularz parametrów (wersja Streamlit)")
+st.title("Przewidywanie bólu")
 
 # =========================
 #        PARAMETRY
@@ -31,48 +31,35 @@ with st.expander("Biometryka", expanded=True):
     col1, col2, col3 = st.columns(3)
 
     with col1:
-        hgh = st.text_input("HgH:", "")
+        hgh = st.text_input("HgH:", "140")
     with col2:
-        hgl = st.text_input("HgL:", "")
+        hgl = st.text_input("HgL:", "95")
     with col3:
-        sen = st.number_input("Sen (h):", min_value=0.0, max_value=24.0, step=0.5)
+        sen = st.number_input("Sen (h: 0-24):", min_value=0, max_value=24, step=1, value=7)
 
-    stres = st.number_input("Stres (1-10):", min_value=1, max_value=10, step=1)
+    stres = st.number_input("Stres (1-10):", min_value=1, max_value=10, step=1,value=2)
 
 # --- Dieta ---
 with st.expander("Dieta", expanded=True):
-    st.subheader("Linia 1")
+    # st.subheader("Linia 1")
     cols = st.columns(5)
     diet_labels1 = ["Mięso białe (%)", "Mięso czerwone (%)", "Nabiał (%)",
                     "Słodycze (g)", "Alkohol (ml)"]
     diet_values1 = []
     for col, label in zip(cols, diet_labels1):
-        diet_values1.append(col.number_input(label, min_value=0.0, step=1.0))
+        diet_values1.append(col.number_input(label, min_value=0, step=1))
 
-    st.subheader("Linia 2")
+    # st.subheader("Linia 2")
     cols = st.columns(5)
     diet_labels2 = ["Warzywa i owoce (%)", "Przetworzone (%)",
                     "Gluten (%)", "Orzechy (garść)", "Kawa (szt.)"]
     diet_values2 = []
     for col, label in zip(cols, diet_labels2):
-        diet_values2.append(col.number_input(label, min_value=0.0, step=1.0))
+        diet_values2.append(col.number_input(label, min_value=0, step=1))
 
 # --- Odczucia ---
 with st.expander("Odczucia", expanded=True):
-    col1, col2, col3 = st.columns(3)
-
-    with col1:
-        bol = st.number_input("Ból (1-10):", min_value=1, max_value=10, step=1)
-
-    with col2:
-        godzina_bolu = st.number_input("Godzina bólu (0-23):", min_value=0, max_value=23, step=1)
-
-    with col3:
-        samopoczucie = st.number_input("Samopoczucie (1-10):", min_value=1, max_value=10, step=1)
-
-# --- Uwagi ---
-with st.expander("Uwagi", expanded=True):
-    uwagi = st.text_area("Uwagi:", height=100)
+    samopoczucie = st.number_input("Samopoczucie (1-10):", min_value=1, max_value=10, step=1,value=10)
 
 # =========================
 #      ZAPIS DANYCH
